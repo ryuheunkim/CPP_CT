5ad587a29c60fe5049a2ede1c0bbf04e14bf44ad
ryuheunkim <ryuheun.kim@gmail.com>
2026-03-31 11:38:34 +0900

    최소값찾기, 스택

--- 04_DataSturcture/Stack/SequenceMaking.cpp
+++ 04_DataSturcture/Stack/SequenceMaking.cpp
@@ -3,10 +3,61 @@
 /*
 ## 발상
 - 자연수 스택, 중간 스택, 결과 기록 스택 (push, pop 순서만 저장)
+  자연수 스택은 필요 없고, 단지 오름차순이기에 ++만 해주면 된다.
 - DataFlow : 자연수 스택 ->(push) 중간 스택 -> (pop)
     1. 중간 스택의 top이 현재 수열의 digit 보다 작으면 (NULL도 작다고 친다), 같아질 때까지 push 한다.
     2. 중간 스택의 top이 현재 수열의 digit과 같으면 pop 한다.
     3. 중간 스택의 top이 현재 수열보다 크면, 수열을 만들 수 없다 (종료)
 ## sudo 코드...
-
 */
+
+# include <iostream>
+# include <vector>
+using namespace std;
+
+int main() {
+    ios :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+    vector <int> S; // 출력전 스택
+    vector <char> P; // 정답 출력 스택
+    bool result = true;
+
+    int num = 1; //오름차순으로 시작하는 자연수(Stack에 집어넣을 수)
+    
+    // 배열 입력
+    for (int i = 0; i < N; i++){
+        int E;
+        cin >> E;
+        
+        while (num <= E){ // top에 입력값이 들어올 때까지 자연수값을 오름차순으로 push
+            S.push_back(num);
+            P.push_back('+');
+            num++;
+        }
+
+        int top = S.size()-1;
+
+        if (S[top] == E){ //top = S.size()-1
+            S.pop_back();
+            P.push_back('-');
+        }
+
+        else if (S[top] > E){
+            result = false;
+        }
+        //while문으로 인해 S[top]가 E보다 작은 경우는 없다.
+    }
+
+    if (result == true){
+        for (int i = 0; i <P.size(); i++){
+            cout << P[i] << "\n";
+        }
+    }
+    else if(result == false){ // 중간에 입력이 끊기면 안돼서 for-문 밖에서 출력
+        cout << "NO" << "\n";
+    }
+}
--- 04_DataSturcture/Vector/Two_Pointer/Find_Min.cpp
+++ 04_DataSturcture/Vector/Two_Pointer/Find_Min.cpp
@@ -5,40 +5,54 @@
 이전에 있던 값 deque.front()의 index가 유효기간(i-L)보다 작으면, (i-L+1)미만이란 뜻, 제거해야 하고
 */
 
-# include <iostream>
-# include <vector>
-# include <deque>
-typedef pair<int, int> Node;
-using namespace std;
+/*  D의 인덱스 확인용 코드
+    
+    int N, L;
+    cin >> N >> L;
+    
+    for (int i = 1; i < N; i++){
+        cout << i << "번째 일 때" << endl;
+        for (int j = i-L+1; j <= i; j++){
+            if (j <= 0){
+                continue;
+            }
+            cout << j << ",";
+        }
+        cout << endl;
+    }
+    
+*/
 
-int main() {
-    ios:: sync_with_stdio(false);
+# include<iostream>
+# include<deque>
+# include <utility>
+using namespace std;
+typedef pair<int,int> Node;
+
+int main(){
+    ios :: sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
     int N, L;
     cin >> N >> L;
 
-    deque<Node> dq;
+    deque <Node> dq;
 
-    for (int i = 0; i < N; i++){
-        long now; // -10^9 ~ 10^9 이기 때문
+    for (int i = 0; i < N; i++){ //문제의 조건의 A_{1}은 A_{0}이니, N회 순회 시킬 것
+        int now;
         cin >> now;
-        // dequq의 뒤의 ele가 현재 입력값보다 크면 --> 제거
-        while (!dq.empty()&& dq.back().value >= now){
-            dq.pop_back();
+        // now 값을 dq에 더할 때
+        while (dq.size() && (dq.back().second > now)){
+            dq.pop_back(); //기존 값들 중 now 보다 큰값들을 다 버림
         }
+        dq.push_back(Node(i,now)); // 앞에 값들이 now 보다 작다면 남아있을 수 있음
 
-        // 값 입력
-        dq.push_back({now, i});
-
-        //윈도우를 벗어나는 첫번째 값 삭제
-        if (dq.front().index < i-L){
+        // 인덱스 범위를 넘어간 min값 제외
+        if (dq.front().first <= i-L){
             dq.pop_front();
         }
 
-        cout << dq.front().value <<"\n";
-
+        cout << dq.front().second << " ";
     }
-
 }
